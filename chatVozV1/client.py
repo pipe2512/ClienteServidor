e1fcad1e767f8bc738d47a562e0af558b77f62bf
andres felipe <pipe251296@gmail.com>
2018-02-19 18:47:48 -0500

    primer intento en el manejo de sesiones

--- chatVozV1/client.py
+++ chatVozV1/client.py
@@ -1,8 +1,97 @@
-#import pyaudio
+import pyaudio
 import wave
 import sys
 import zmq
 import socket
+import threading
+
+def pruebaReproducir(frames):
+    CHUNK = 1024
+
+    #wf = wave.open("paso.wav", 'rb')
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format = pyaudio.paInt16,
+                    channels = 2,
+                    rate = 44100,
+                    output=True)
+
+    #data = wf.readframes(CHUNK)
+
+    #for i in range(220160):
+    #    print("estoy entrando")
+    stream.write(frames)
+        #data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+def grabarAudio():
+    #Parametros para el manejo del audio
+    CHUNK = 1024
+    RECORD_SECONDS = 5
+
+    #Instanciamos pyaudio
+    p = pyaudio.PyAudio()
+
+    #Creamos el stream para grabar el audio
+    stream = p.open(format = pyaudio.paInt16,
+                    channels = 2,
+                    rate = 44100,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    frames = []
+    numeroFrames = int(RATE / CHUNK * RECORD_SECONDS)
+
+    print("Grabando")
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("******")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    retorno = (b''.join(frames))
+    return retorno
+
+def recepcionMensja(banderaOcupado):
+    msg = p.recv_json()
+    if msg["operacion"] == "ocupado":
+        if banderaOcupado == "0" and msg["estado"] == "0":
+            p.send_json({"resultado" : "conectado"})
+            banderaOcupado = "1"
+        elif banderaOcupado == "1" and msg["estado"] == "0":
+            p.send_json({"resultado" : "ocupado"})
+        elif banderaOcupado == "1" and msg["estado"] == "1":
+            p.send_json({"resultado" : "desconectado"})
+            banderaOcupado = "0"
+
+def menu(banderaOcupado):
+    print("Seleccione una opcion: ")
+    print ("1.Conectarme a otro cliente")
+    print("2.Salir del chat")
+    opcion = input()
+    if opcion == 1 and banderaOcupado == "0":
+        print("Ingrese el alias del cliente")
+        s.send_json({"operation" : "clientes"})
+        msg = s.recv_json()
+        print(msg["clientes"])
+        opcion2 = input()
+        s.send_json({"conexion" : opcion2})
+        msg = s.recv_json()#Mensaje de okey
+        if msg["resultado"] == "conectado":
+            banderaOcupado = "1"
+    elif opcion == 1 and banderaOcupado == "1":
+        print("Usted se encuentra conectado con otro cliente")
+    elif opcion == 2 and banderaOcupado == "1":
+        print("pendiente.......")
+    else:
+        print("No se encuentra conectado con nadie")
 
 
 def main():
@@ -16,25 +105,23 @@ def main():
     ipPropia = sys.argv[3]#Ip propia
     puertoPropio = sys.argv[4]#Puerto propio
     alias = sys.argv[5]#Alias del cliente
+    banderaOcupado = "0"
 
     #Conexion con el servidor.
     context = zmq.Context()
     s = context.socket(zmq.REQ)
     s.connect("tcp://{}:{}".format(ip, port))
 
-    #Conexion primera vez al servidor
-    s.send_json({"ip" : ipPropia, "puerto" : puertoPropio, "alias" : alias, "operacion" : "registro"})
-
     #Creacion del socket con el cual se conectara el servidor.
     context2 = zmq.Context()
     p = context2.socket(zmq.REP)
     p.bind("tcp://*:{}".format(puertoPropio))
 
-
-    while True:
-        msg = p.recv_json()
-
-
+    #Conexion primera vez al servidor
+    s.send_json({"ip" : ipPropia, "puerto" : puertoPropio, "alias" : alias, "operacion" : "registro"})
+    print("elegir el ciente con el que se va a conectar: ")
+    msg = s.recv_json()#Mensaje de okey
+    print(msg["clientes"])
 
 
 if __name__ == '__main__':
--- chatVozV1/server.py
+++ chatVozV1/server.py
@@ -1,4 +1,4 @@
-#import pyaudio
+import pyaudio
 import wave
 import sys
 import zmq
@@ -11,17 +11,32 @@ def main():
 
     clientes = []
     port = sys.argv[1]#Puerto del servidor
+
+    #Creacion del socket para escuchar los clientes
     context = zmq.Context()
     s = context.socket(zmq.REP)
     s.bind("tcp://*:{}".format(port))
+
+    #Diccionario de clientes conectados
+    clientes = {}
+
     while True:
         msg = s.recv_json()
         if msg["operacion"] == "registro":
             context2 = zmq.Context()
             p = context2.socket(zmq.REQ)
             p.connect("tcp://{}:{}".format(msg["ip"], msg["puerto"]))
-            clientes.append(p)
-            print("Registrado el cliente {}".format(msg["alias"]))
+            s.send_json({"okey" : "okey"})
+            clientes = {msg["alias"] : [p, "0"]}#Socket y bandera de estado(Ocupado 1/Libre 0)
+        if msg["operacion"] == "clientes":
+            s.send_json({"clientes" : list(cliente.keys())})
+        if msg["operacion"] == "conexion":
+            clientes[msg["conexion"]].send_json({"operacion" : "ocupado", "estado" : "0"})
+            msg = clientes[msg["conexion"]].recv_json()
+            s.send_json(msg)####Tener en cuenta###### Puede ser un error
+
+
+
 
 if __name__ == "__main__":
     main()
